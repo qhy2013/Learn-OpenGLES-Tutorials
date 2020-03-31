06854a424a385bc4d7ca562a374d325980bead51
yang3 <2824303813@qq.com>
2020-03-31 16:01:16 +0800

    第八课：添加注释(Matrix.setLookAtM(), Matrix.frustumM())

--- android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8/LessonEightGLSurfaceView.java
+++ android/AndroidOpenGLESLessons/app/src/main/java/com/learnopengles/android/lesson8/LessonEightGLSurfaceView.java
@@ -8,88 +8,78 @@ import android.widget.Toast;
 
 import com.learnopengles.android.R;
 
-public class LessonEightGLSurfaceView extends GLSurfaceView implements ErrorHandler
-{	
-	private LessonEightRenderer renderer;
-	
-	// Offsets for touch events	 
+public class LessonEightGLSurfaceView extends GLSurfaceView implements ErrorHandler {
+    private LessonEightRenderer renderer;
+
+    // Offsets for touch events
     private float previousX;
     private float previousY;
-    
+
     private float density;
-        	
-	public LessonEightGLSurfaceView(Context context) 
-	{
-		super(context);		
-	}
-	
-	public LessonEightGLSurfaceView(Context context, AttributeSet attrs) 
-	{
-		super(context, attrs);		
-	}
-	
-	@Override
-	public void handleError(final ErrorType errorType, final String cause) {
-		// Queue on UI thread.
-		post(new Runnable() {
-			@Override
-			public void run() {
-				final String text;
 
-				switch (errorType) {
-				case BUFFER_CREATION_ERROR:
-					text = String
-							.format(getContext().getResources().getString(
-									R.string.lesson_eight_error_could_not_create_vbo), cause);
-					break;
-				default:
-					text = String.format(
-							getContext().getResources().getString(
-									R.string.lesson_eight_error_unknown), cause);
-				}
+    public LessonEightGLSurfaceView(Context context) {
+        super(context);
+    }
 
-				Toast.makeText(getContext(), text, Toast.LENGTH_LONG).show();
+    public LessonEightGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-			}
-		});
-	}
+    @Override
+    public void handleError(final ErrorType errorType, final String cause) {
+        // Queue on UI thread.
+        post(new Runnable() {
+            @Override
+            public void run() {
+                final String text;
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) 
-	{
-		if (event != null)
-		{			
-			float x = event.getX();
-			float y = event.getY();
-			
-			if (event.getAction() == MotionEvent.ACTION_MOVE)
-			{
-				if (renderer != null)
-				{
-					float deltaX = (x - previousX) / density / 2f;
-					float deltaY = (y - previousY) / density / 2f;
-					
-					renderer.deltaX += deltaX;
-					renderer.deltaY += deltaY;												
-				}
-			}	
-			
-			previousX = x;
-			previousY = y;
-			
-			return true;
-		}
-		else
-		{
-			return super.onTouchEvent(event);
-		}		
-	}
+                switch (errorType) {
+                    case BUFFER_CREATION_ERROR:
+                        text = String
+                                .format(getContext().getResources().getString(
+                                        R.string.lesson_eight_error_could_not_create_vbo), cause);
+                        break;
+                    default:
+                        text = String.format(
+                                getContext().getResources().getString(
+                                        R.string.lesson_eight_error_unknown), cause);
+                }
 
-	// Hides superclass method.
-	public void setRenderer(LessonEightRenderer renderer, float density) 
-	{
-		this.renderer = renderer;
-		this.density = density;
-		super.setRenderer(renderer);
-	}
+                Toast.makeText(getContext(), text, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event != null) {
+            float x = event.getX();
+            float y = event.getY();
+
+            if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                if (renderer != null) {
+                    float deltaX = (x - previousX) / density / 2f;
+                    float deltaY = (y - previousY) / density / 2f;
+
+                    renderer.deltaX += deltaX;
+                    renderer.deltaY += deltaY;
+                }
+            }
+
+            previousX = x;
+            previousY = y;
+
+            return true;
+        } else {
+            return super.onTouchEvent(event);
+        }
+    }
+
+    // Hides superclass method.
+    public void setRenderer(LessonEightRenderer renderer, float density) {
+        this.renderer = renderer;
+        this.density = density;
+        super.setRenderer(renderer);
+    }
 }
